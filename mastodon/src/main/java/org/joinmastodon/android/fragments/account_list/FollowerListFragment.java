c4b8a11e2e9ca0ca3565d91fad5d16e19e1dbc8d
LucasGGamerM <moshidon.app@gmail.com>
2025-04-07 11:09:36 -0300

    refactor(FollowerListFragment.java): add getWebUri method back

--- mastodon/src/main/java/org/joinmastodon/android/fragments/account_list/FollowerListFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/account_list/FollowerListFragment.java
@@ -1,5 +1,6 @@
 package org.joinmastodon.android.fragments.account_list;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import org.joinmastodon.android.R;
@@ -18,5 +19,11 @@ public class FollowerListFragment extends AccountRelatedAccountListFragment{
 	@Override
 	public HeaderPaginationRequest<Account> onCreateRequest(String maxID, int count){
 		return new GetAccountFollowers(account.id, maxID, count);
+	}
+
+	// MOSHIDON:
+	@Override
+	public Uri getWebUri(Uri.Builder base) {
+		return null;
 	}
 }
