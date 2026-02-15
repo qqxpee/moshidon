95f31c894c5b8fdfc093b977ad9d86f7b114f123
LucasGGamerM <LucasGGamerM@protonmail.com>
2026-02-15 19:48:18 -0300

    fix(StatusEditHistoryFragment): use our own reblog or reply line implementation

--- mastodon/src/main/java/org/joinmastodon/android/fragments/StatusEditHistoryFragment.java
+++ mastodon/src/main/java/org/joinmastodon/android/fragments/StatusEditHistoryFragment.java
@@ -160,7 +160,7 @@ public class StatusEditHistoryFragment extends StatusListFragment{
 					action=getString(R.string.edit_multiple_changed);
 				}
 			}
-			items.add(0, new ReblogOrReplyLineStatusDisplayItem(s.id, this, getActivity(), action+" · "+date, null, 0, accountID));
+			items.add(0, new ReblogOrReplyLineCustomStatusDisplayItem(s.id, this, getActivity(), action+" · "+date, null, 0));
 		}
 		return items;
 	}
